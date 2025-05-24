c35199986f83559268d713787b3ea8da0f7f94e4
gerusura <gerusuraimu@mail.gerusuraimu.com>
2025-05-24 16:13:19 +0900

    import fix

--- whiteboardlm/Common/__init__.py
+++ whiteboardlm/Common/__init__.py
@@ -1,6 +1,5 @@
 from .Logger import get_logger
 from .Respond import respond
-from .Respond import respond_test
 from .GetToken import get_token
 from .Payload import UIDPayload
 from .Payload import EmbedRequest
